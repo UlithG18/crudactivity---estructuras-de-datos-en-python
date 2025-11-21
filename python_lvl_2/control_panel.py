e5ef44d36d9b166942bd8f0e210bd89318032766
Ulith <ulith.software@gmail.com>
2025-11-20 21:48:51 -0500

    Delete python_lvl_2 directory

--- python_lvl_2/control_panel.py
+++
@@ -1,8 +0,0 @@
-# P10. Panel de control doméstico
-# Desarrolla un sistema por consola que simule el control de una vivienda:
-
-# Menú con opciones para encender luces, activar calefacción, ver estado o salir.
-# Las luces solo pueden encenderse si es de noche.
-# La calefacción solo puede activarse si la temperatura es menor a 18 °C y las luces están encendidas.
-# El sistema debe conservar el estado entre acciones y validar combinaciones imposibles.
-# Incluye un diagrama de flujo que muestre la lógica de las condiciones principales.
